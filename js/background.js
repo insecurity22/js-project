5ac0da698f0d9ea3cedce7608b39a07e452c359f
insecurity22 <insecurity22@naver.com>
2021-08-09 05:10:19 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -3,4 +3,4 @@ const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const body = document.querySelector("body");
-body.style.backgroundImage = `url(../img/${chosenImage})`;
+body.style.backgroundImage = `url(./img/${chosenImage})`;
